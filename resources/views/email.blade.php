3b9e165337c4ef155dd8360bd7972363e3d5c14a
Unknown <yiseca@gmail.com>
2017-11-10 11:06:11 -0500

    formulario las delicias

---
+++ resources/views/email.blade.php
@@ -0,0 +1,15 @@
+<html>
+
+
+
+<h2>Caminata RECREO ECOLÓGICA QUEBRADA LAS DELICIAS<br>Domingo 12 de Noviembre de 2017.</h2>
+
+<h3>Punto de encuentro: Cra 7 Calle 57 – Costado Oriental </h3>
+
+<h3>Hora de salida: 8:00am</h3>
+
+<h1>Su registro fue exitoso!</h1>
+
+
+
+<p></p>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,256 @@
+@extends('master')                              
+
+@section('content')         
+
+<link rel="stylesheet" type="text/css" href="public/Css/form.css">
+
+<link rel="stylesheet" type="text/css" href="public/Css/slider_styles.css">
+
+<section id="page1">
+
+    <div class="panel panel-default">
+
+        <div class="panel-heading" style="font-family: arial; font-size: 18px"><center>CAMINATA RECREO ECOLÓGICA
+QUEBRADA LAS DELICIAS<br>Domingo 12 de Noviembre de 2017 </center></div>
+
+        <div class="panel-body">
+
+            <div class="freebirdFormviewerViewFormContent ">
+
+                <div class="freebirdFormviewerViewHeaderHeader">
+
+                    <div class="freebirdFormviewerViewHeaderTitleRow">
+
+                    </div>
+<br>
+                    <div class="stage">
+
+                        <div id="SLDR-ONE" class="sldr">
+
+                            <ul class="wrp animate">
+
+                                <li class="elmnt-one">                                 
+
+                                        <div class="wrap"><img src="public/Img/deli.jpg"  height="400">
+
+                                        </div>                                   
+                                </li>
+
+                               <li class="elmnt-two">
+                                    
+                                        <div class="wrap"><img src="public/Img/deli1.jpg"  height="400">
+
+                                        </div>                                 
+                                </li>
+
+                                 <li class="elmnt-three">
+                                    
+                                        <div class="wrap"><img src="public/Img/rio.jpg"  height="400">
+
+                                        </div>                                 
+                                </li>
+                                 
+
+                            </ul>
+
+                        </div>
+
+                        <div class="clear"></div>
+
+                        <ul class="selectors">
+
+                            <li class="focalPoint"><a href="">•</a></li>
+
+                            <li><a href="">•</a></li>
+
+                            <li><a href="">•</a></li>            
+                         
+                        </ul>
+
+                    </div>
+
+                    <script src="public/Js/jquery.sldr.js"></script>
+
+                    <div class="freebirdFormviewerViewHeaderDescription" dir="auto">
+
+
+                        <h4>Ubicación</h4>
+
+                        <p align="justify" style="font-size: 11pt; line-height: 25px">La Quebrada Las Delicias está ubicada en la localidad de Chapinero, Es un lugar insignia de la ecología bogotana por su proceso de recuperación Integral, donde la comunidad de sectores vecinos como los Barrios: Bosque Calderón y Pardo Rubio se han involucrado en su conservación. Es la caída de agua más alta que tiene la ciudad de Bogotá y se pueden visitar varios pozos de agua totalmente limpia.</p>
+                       
+                        <h4>Descripción del recorrido </h4>
+
+                        <p align="justify" style="font-size: 11pt; line-height: 25px">El recorrido da inicio en la carrera séptima con calle 57, costado oriental y se recorre la zona residencial hasta el parqueadero del Politécnico Gran Colombiano, Donde encontramos el ingreso al sendero y el primer pozo “La Virgen” subiendo un poco más encontramos un fabuloso mirador y a 30 min. Aproximadamente esta la caída de agua.</p>
+    
+                        <br>
+
+                        <font size="3"><center><table width="900">
+
+                            <col width="281">
+
+                                <col width="282">
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">Punto de encuentro</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">Cra 7 Calle 57 – Costado Oriental</p>
+
+                                        </td>
+
+                                    </tr>
+                              
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">Fecha</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">Domingo 12 de Noviembre de 2017</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">Hora de encuentro</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">8:00 a.m</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">Cupo Máximo</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">40 Personsa</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">Rango de Edad</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">12-60 años</p>
+
+                                        </td>
+
+                                    </tr>
+   
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">Tiempo de recorrido</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">4 horas</p>
+
+                                        </td>
+
+                                    </tr> 
+
+                                     <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">Altitud</td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">Hasta 2.900  m.s.n.m</p>
+
+                                        </td>
+
+                                    </tr>
+   
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">Temperatura</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p style="font-size: 11pt">14°</p>
+
+                                        </td>
+
+                                    </tr> 
+
+                        </table></center></font>
+                        <h4>Recomendaciones</h4>
+
+                        <ul class="ul_inicio">
+
+                            <font  style="font-size: 11pt; line-height: 25px">
+
+                                <li>Llevar ropa cómoda y  de cambio si desea ingresar al agua</li>
+                            
+                                <li>Llevar bloqueador solar</li>
+
+                                <li>Hidratación</li>
+
+                                <li>Calzado cómodo</li>
+
+                                <li>Medicamentos personales</li>
+
+                                <li> Abstenerse de participar: Mujeres en estado de embarazo, Personas con problemas de salud, Mayores de 60 años y menores de 12 años.</li>
+
+                                <li><ins>No se permiten mascotas</ins></li>
+
+                                <li><strong> Abstenerse de asistir si no se encuentra inscrito</strong></li>
+
+                            </font>
+
+                            <br>
+
+                        </ul> <a class="btn btn-info" href="registro">Registrarse</a> </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+</section>
+
+@stop
